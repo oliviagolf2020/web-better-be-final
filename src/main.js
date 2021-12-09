256a3ecd6daf981b34e92bdceeefd22a0ac6c9f3
Olivia Worsham <Olivia.Worsham@sckans.edu>
2021-12-08 19:35:59 -0600

    final

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Image from "../src/image1.svg";
+
+const Header = () => {
+    return(
+        <>
+            <div>
+                <img className="main" src={Image}/>
+            </div>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/second.js
@@ -0,0 +1,13 @@
+import Image from "../src/image2.svg";
+
+const Header2 = () => {
+    return(
+        <>
+            <div>
+                <img className="main1" src={Image}/>
+            </div>
+        </>
+    )
+}
+
+export default Header2;
